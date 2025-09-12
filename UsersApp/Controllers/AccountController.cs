31442b81589dc05637128834a768d5b9a52d15fa
lvibar <lvibar@DESKTOP-VIBAR>
2025-09-12 13:20:55 +0800

    Add project files.

---
+++ UsersApp/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UsersApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult VerifyEmail()
+        {
+            return View();
+        }
+    }
+}
---
+++ UsersApp/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UsersApp.Data
+{
+    public class AppDbContext : IdentityDbContext<Models.Users>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected AppDbContext()
+        {
+        }
+    }
+}
